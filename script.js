57f434309e79bafeb3aa13252ddd09884b43ea1a
oliver <oliveralexander00@gmail.com>
2016-10-02 08:40:57 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function divide(num){
+	return num/num;
+}
+
+
+function multiply(num){
+	for(var i =0; i<13; i++){
+  	num*=2;
+  }
+  return num;
+}
+
+function add(num){
+	return Math.ceil(num/1000)*1000;
+}
+
+alert(add(multiply(divide())));
